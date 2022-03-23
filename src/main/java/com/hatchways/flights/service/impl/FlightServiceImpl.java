1ac5431f2fc641201c7013ac00542469319b9c7a
unknown <ha291921@ens.uqam.ca>
2022-03-23 12:13:53 -0400

    flight service implementation

---
+++ src/main/java/com/hatchways/flights/service/impl/FlightServiceImpl.java
@@ -0,0 +1,20 @@
+package com.hatchways.flights.service.impl;
+
+import com.hatchways.flights.rest.request.RequestEvent;
+import com.hatchways.flights.rest.response.Response;
+import com.hatchways.flights.service.FlightService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FlightServiceImpl implements FlightService {
+    @Override
+    public ResponseEntity<Response> processTicket(RequestEvent requestEvent) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Response> getFlightsInfos(String startDate, String endDate) {
+        return null;
+    }
+}
