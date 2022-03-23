6f7d7806306eb46d8840f7d0366c411022832f7b
unknown <ha291921@ens.uqam.ca>
2022-03-23 12:16:24 -0400

    REST controller

--- src/main/java/com/hatchways/flights/rest/HatchwaysFlightRest.java
+++ src/main/java/com/hatchways/flights/rest/HatchwaysFlightRest.java
@@ -1,10 +1,30 @@
 package com.hatchways.flights.rest;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hatchways.flights.rest.request.RequestEvent;
+import com.hatchways.flights.rest.response.Response;
+import com.hatchways.flights.service.FlightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
 public class HatchwaysFlightRest {
+
+    @Autowired
+    FlightService flightService;
+
+    @PostMapping("/tickets")
+    public ResponseEntity<Response> processTicket(@Valid @RequestBody RequestEvent requestEvent){
+        return flightService.processTicket(requestEvent);
+    }
+
+    @GetMapping("/flights")
+    public ResponseEntity<Response> getFlightsInfos(@RequestParam(required = false) String startDate,
+                                                    @RequestParam(required = false) String endDate){
+        return null;
+    }
 }
