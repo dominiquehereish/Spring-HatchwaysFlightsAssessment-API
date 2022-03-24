6ca266da325ff00b4fcb696fa74644287d3b4897
unknown <ha291921@ens.uqam.ca>
2022-03-24 18:00:24 -0400

    @Entity

--- src/main/java/com/hatchways/flights/model/Flight.java
+++ src/main/java/com/hatchways/flights/model/Flight.java
@@ -1,10 +1,25 @@
 package com.hatchways.flights.model;
 
-import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "flights")
 public class Flight {
 
+    @Id
+    @Column(nullable = false, unique = true, updatable = false)
     private String flightNumber;
+    @Column(nullable = false)
     private LocalDate flightDate;
+    @OneToMany
+    private Set<Ticket> tickets;
 
 }
--- src/main/java/com/hatchways/flights/model/Ticket.java
+++ src/main/java/com/hatchways/flights/model/Ticket.java
@@ -1,9 +1,23 @@
 package com.hatchways.flights.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "Tickets")
 public class Ticket {
 
+    @Id
+    @Column(nullable = false, unique = true, updatable = false)
     private int ticketId;
+    @Column(nullable = false)
     private String seatNumber;
+    @Column(nullable = false)
     private int ticketCost;
 
 }
