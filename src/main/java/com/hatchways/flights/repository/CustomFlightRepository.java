614c6709b87bf186ab17a43421a3e9785205834f
unknown <ha291921@ens.uqam.ca>
2022-03-26 18:36:53 -0400

    custom flight repository

--- src/main/java/com/hatchways/flights/repository/CustomFlightRepository.java
+++ src/main/java/com/hatchways/flights/repository/CustomFlightRepository.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Repository
-public interface FlightRep {
+public interface CustomFlightRepository {
 
     Flight findById(String id);
     Flight save(Flight flight);
