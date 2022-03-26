6b9a5ce6a23c415f000f410e0afbef79dae47017
unknown <ha291921@ens.uqam.ca>
2022-03-26 18:34:45 -0400

    custom flight repository

---
+++ src/main/java/com/hatchways/flights/repository/FlightRep.java
@@ -0,0 +1,17 @@
+package com.hatchways.flights.repository;
+
+import com.hatchways.flights.model.Flight;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface FlightRep {
+
+    Flight findById(String id);
+    Flight save(Flight flight);
+    List<Flight> findByFlightDate(LocalDate date);
+    List<Flight> findByPeriod(LocalDate startDate, LocalDate endDate);
+
+}
