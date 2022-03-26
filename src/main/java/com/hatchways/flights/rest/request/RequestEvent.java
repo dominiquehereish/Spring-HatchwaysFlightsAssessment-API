06ce2e60cf546b0d7056981fc9943da87db7b5f2
unknown <ha291921@ens.uqam.ca>
2022-03-26 18:41:41 -0400

    RequestEvent model

--- src/main/java/com/hatchways/flights/rest/request/RequestEvent.java
+++ src/main/java/com/hatchways/flights/rest/request/RequestEvent.java
@@ -3,9 +3,12 @@ package com.hatchways.flights.rest.request;
 import com.hatchways.flights.model.Event;
 import lombok.Data;
 
+import javax.validation.Valid;
+
 @Data
 public class RequestEvent {
 
+    @Valid
     private Event event;
 
 }
