b8c818d503ed08368b4609bdbba214f104eb86e0
unknown <ha291921@ens.uqam.ca>
2022-03-26 18:38:36 -0400

    repository

--- src/main/java/com/hatchways/flights/repository/FlightRepository.java
+++ src/main/java/com/hatchways/flights/repository/FlightRepository.java
@@ -4,6 +4,11 @@ import com.hatchways.flights.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
-public interface FlightRepository extends JpaRepository<Flight, String> {
+public interface FlightRepository extends JpaRepository<Flight, String>{
+
+    List<Flight> findByFlightDate(LocalDate date);
 }
