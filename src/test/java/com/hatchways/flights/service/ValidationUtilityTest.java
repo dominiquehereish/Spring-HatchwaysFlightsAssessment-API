b9b4ade03977a26f96950846c111e9b1021b7b37
unknown <ha291921@ens.uqam.ca>
2022-03-27 14:09:06 -0400

    test

---
+++ src/test/java/com/hatchways/flights/service/ValidationUtilityTest.java
@@ -0,0 +1,16 @@
+package com.hatchways.flights.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidationUtilityTest {
+
+    @Test
+    void isEmptyDate() {
+    }
+
+    @Test
+    void isValidDate() {
+    }
+}
