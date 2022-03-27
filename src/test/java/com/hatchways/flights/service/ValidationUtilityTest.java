c8e42df1eb7c1ee6ea3c7dcafb2ac58581062851
unknown <ha291921@ens.uqam.ca>
2022-03-27 14:17:34 -0400

    tests

--- src/test/java/com/hatchways/flights/service/ValidationUtilityTest.java
+++ src/test/java/com/hatchways/flights/service/ValidationUtilityTest.java
@@ -6,11 +6,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ValidationUtilityTest {
 
+    private String nullDate = null;
+    private String emptyDate = "";
+    private String validDate = "2021-11-01";
+
     @Test
     void isEmptyDate() {
+        assertTrue(ValidationUtility.isEmptyDate(nullDate), "is null date");
+        assertTrue(ValidationUtility.isEmptyDate(emptyDate), "is empty date");
+    }
+
+    @Test
+    void isNotEmptyDate() {
+        assertFalse(ValidationUtility.isEmptyDate(validDate), "is not empty date");
     }
 
     @Test
     void isValidDate() {
+        assertTrue(ValidationUtility.isValidDate(validDate), "is a valid date");
+    }
+
+    @Test
+    void isNotValidDate() {
+        assertFalse(ValidationUtility.isValidDate(nullDate), "is not a valid date");
+        assertFalse(ValidationUtility.isValidDate(emptyDate), "is not a valid date");
     }
 }
